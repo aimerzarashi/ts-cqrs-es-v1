90440638bcd0512ca3f940c03a27413f6674ba15
aimerzarashi <git.aimerzarashi@gmail.com>
2023-09-03 20:25:35 +0900

    🧱 Changed namespace from sandbox to e2etest

--- test-runner/playwright/my-app/tests/example.spec.ts
+++ test-runner/playwright/my-app/tests/example.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://web.dev.localhost/');
 //  await page.goto('http://web-ui:3000');
+//  await page.goto('http://web-ui.sandbox.svc.cluster.local:3000');
+  await page.goto('https://web.dev.localhost/');
 
   await expect(page).toHaveTitle(/Create Next App/);
   await expect(page).toHaveScreenshot();
