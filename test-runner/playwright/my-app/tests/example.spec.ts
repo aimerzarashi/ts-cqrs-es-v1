ade0205da3f476977246cbe1ea7c47319d3c63d2
aimerzarashi <git.aimerzarashi@gmail.com>
2023-09-04 21:37:02 +0900

    ✅ added https tests

--- test-runner/playwright/my-app/tests/example.spec.ts
+++ test-runner/playwright/my-app/tests/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://web-ui.sandbox.svc.cluster.local:3000');
-//  await page.goto('https://web-ui.dev.localhost/');
+//  await page.goto('http://web-ui.sandbox.svc.cluster.local:3000');
+  await page.goto('https://web-ui.dev.localhost/');
 
   await expect(page).toHaveTitle(/Create Next App/);
   await expect(page).toHaveScreenshot();
