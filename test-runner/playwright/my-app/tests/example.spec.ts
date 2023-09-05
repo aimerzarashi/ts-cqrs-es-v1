19c60be2babfdd594c7301da17dcf0f5dc555a05
aimerzarashi <git.aimerzarashi@gmail.com>
2023-09-05 21:39:25 +0900

    ✅ Changed test code for the change of top page

--- test-runner/playwright/my-app/tests/example.spec.ts
+++ test-runner/playwright/my-app/tests/example.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-//  await page.goto('http://web-ui.sandbox.svc.cluster.local:3000');
   await page.goto('https://web-ui.dev.localhost/');
-
-  await expect(page).toHaveTitle(/Create Next App/);
+  await expect(page).toHaveTitle('CQRS/ESで業務システムをつくろう');
   await expect(page).toHaveScreenshot();
+});
+
+test('has topMenu', async ({ page }) => {
+    await page.goto('https://web-ui.dev.localhost/');
+    const topMenu = page.locator('.topMenu');
+    await expect(topMenu).toHaveText('TOP MENU');
 });
