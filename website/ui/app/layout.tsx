b2f9e45f0c989b3c8ecb3a5fa325b15a7fd204e3
aimerzarashi <git.aimerzarashi@gmail.com>
2023-09-12 22:06:44 +0900

    ♻️ changed auth provider position

--- website/ui/app/layout.tsx
+++ website/ui/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+        </body>
       </AuthProvider>
     </html>
   );
