30998de281fe48fc973ff9b46a518085d56d0269
aimerzarashi <git.aimerzarashi@gmail.com>
2023-09-13 21:24:09 +0900

    ✅ renamed "example" to "top"

--- website/tests/tests/top.spec.ts
+++ website/tests/tests/top.spec.ts
@@ -1,13 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://web-ui.dev.localhost/');
+  await page.goto('https://website-ui.dev.localhost/');
   await expect(page).toHaveTitle('CQRS/ESで業務システムをつくろう');
-  await expect(page).toHaveScreenshot();
 });
 
 test('has topMenu', async ({ page }) => {
-    await page.goto('https://web-ui.dev.localhost/');
+    await page.goto('https://website-ui.dev.localhost/');
     const topMenu = page.locator('.topMenu');
     await expect(topMenu).toHaveText('TOP MENU');
+});
+
+test('regression test', async ({ page }) => {
+  await page.goto('https://website-ui.dev.localhost/');
+  await expect(page).toHaveScreenshot();
 });
