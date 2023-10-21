298806eb699673cb5edcdddfcb44311bc10cb24e
aimerzarashi <git.aimerzarashi@gmail.com>
2023-10-21 19:55:38 +0900

    ✅ Completed token test

--- website/test/tests/auth/token.spec.ts
+++ website/test/tests/auth/token.spec.ts
@@ -20,7 +20,7 @@ test.beforeAll(async () => {
 
 test('iam user registration', async ({ page }) => {
   await page.goto(BASE_URL.UI + '/api/auth/signout');
-  await page.getByRole('button', { name: 'Sign out' });
+  await page.getByRole('button', { name: 'Sign out' }).click();
 
   await page.goto(BASE_URL.UI + '/api/auth/signin');
   await expect(page).toHaveTitle('Sign In');
@@ -32,6 +32,74 @@ test('iam user registration', async ({ page }) => {
   const page1Promise = page.waitForEvent('popup');
   await page.frameLocator('#preview-html').getByRole('link', { name: 'Sign in' }).click();
   const page1 = await page1Promise;
+  page1.close();
+
+  await page.goto(BASE_URL.IAM);
+  await page.getByRole('link', { name: 'Administration Console' }).click();
+
+  await page.getByLabel('Username or email').fill('admin');
+  await page.getByLabel('Username or email').press('Tab');
+  await page.getByLabel('Password').fill('password');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  await page.getByTestId('realmSelectorToggle').click();
+  await page.getByRole('menuitem', { name: 'aimerzarashi' }).click();
+
+  await page.getByRole('link', { name: 'Users' }).click();
+  await page.getByPlaceholder('Search user').click();
+  await page.getByPlaceholder('Search user').fill(email);
+  await page.getByLabel('Search', { exact: true }).click();
+  await expect(page.getByRole('gridcell', { name: email }).nth(1)).toBeAttached();
+
+  await page.getByRole('link', { name: email }).click();
+  await expect(page.getByRole('textbox', { name: 'Username' })).toHaveValue(email);
+  await expect(page.getByRole('textbox', { name: 'Email' })).toHaveValue(email);
+
+  await page.getByTestId('user-sessions-tab').click();
+  await expect(page.getByRole('link', { name: 'website-ui' })).toBeAttached();
+});
+
+test('iam user logout', async ({ page }) => {
+  await page.goto(BASE_URL.UI + '/api/auth/signout');
+  await page.getByRole('button', { name: 'Sign out' }).click();
+
+  await page.goto(BASE_URL.IAM);
+  await page.getByRole('link', { name: 'Administration Console' }).click();
+
+  await page.getByLabel('Username or email').fill('admin');
+  await page.getByLabel('Username or email').press('Tab');
+  await page.getByLabel('Password').fill('password');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  await page.getByTestId('realmSelectorToggle').click();
+  await page.getByRole('menuitem', { name: 'aimerzarashi' }).click();
+
+  await page.getByRole('link', { name: 'Users' }).click();
+  await page.getByPlaceholder('Search user').click();
+  await page.getByPlaceholder('Search user').fill(email);
+  await page.getByLabel('Search', { exact: true }).click();
+  await expect(page.getByRole('gridcell', { name: email }).nth(1)).toBeAttached();
+
+  await page.getByRole('link', { name: email }).click();
+  await expect(page.getByRole('textbox', { name: 'Username' })).toHaveValue(email);
+  await expect(page.getByRole('textbox', { name: 'Email' })).toHaveValue(email);
+
+  await page.getByTestId('user-sessions-tab').click();
+  await expect(page.getByRole('link', { name: 'website-ui' })).not.toBeAttached();
+});
+
+test('iam user relogin', async ({ page }) => {
+  await page.goto(BASE_URL.UI + '/api/auth/signin');
+  await expect(page).toHaveTitle('Sign In');
+  await page.getByRole('textbox', { name: 'Email' }).fill(email);
+  await page.getByRole('button', { name: 'Sign in with Email' }).click();
+
+  await page.goto(BASE_URL.SMTP);
+  await page.getByText(email).nth(1).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.frameLocator('#preview-html').getByRole('link', { name: 'Sign in' }).click();
+  const page1 = await page1Promise;
+  page1.close();
 
   await page.goto(BASE_URL.IAM);
   await page.getByRole('link', { name: 'Administration Console' }).click();
