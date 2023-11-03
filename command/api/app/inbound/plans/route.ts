6912e708bfbea9eb2aa0047495f30e15ab63f85d
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-03 09:40:02 +0900

    🔨 added new endpoint

---
+++ command/api/app/inbound/plans/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from 'next/headers';
+import { extractAccountId } from '@/lib/auth/validation';
+import { paths } from '@/types/openapi';
+
+type RequestBody = paths['/inbound/plans']['post']['requestBody']['content']['application/json'];
+
+export async function POST(request: NextRequest) {
+  const headersList = headers();
+  const authorization = headersList.get('authorization');
+
+  const accountId = extractAccountId(authorization);
+  if (accountId.kind == 'error') {
+    return NextResponse.json(
+      { error: accountId.error.message },
+      { status: 401 }
+    );
+  }
+
+  const requestbody: RequestBody = await request.json();
+  console.debug({ accountId: accountId, requestbody: requestbody });
+
+  return NextResponse.json({ message: 'success' }, { status: 201 });
+}
