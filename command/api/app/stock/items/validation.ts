3db17f411447a0a2c1ee02d663cdcb1c42beba73
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-04 10:14:06 +0900

    🔨 Added input validation for stock item API

---
+++ command/api/app/stock/items/validation.ts
@@ -0,0 +1,22 @@
+import { Result, createSuccess, createError, pipe } from '@/lib/function/resultType';
+import { paths } from '@/types/openapi';
+
+type RequestBody = paths['/stock/items']['post']['requestBody']['content']['application/json'];
+type StockItemCommand = {
+  type: 'valid';
+  body: RequestBody;
+}
+export function validation(request: RequestBody): Result<StockItemCommand> {
+  return pipe(
+    request,
+    validateName
+  );
+}
+
+function validateName(request: RequestBody): Result<StockItemCommand> {
+  if (!request.name) {
+    return createError(new Error('name is required'), request);
+  }
+
+  return createSuccess({ type: 'valid', body: request });
+}
