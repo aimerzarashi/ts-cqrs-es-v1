99a6eb1138cfa117b029a3b175bd6330ab26d491
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-04 10:15:08 +0900

    🔨 added input validation & persistence

--- command/api/app/stock/items/route.ts
+++ command/api/app/stock/items/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers';
 import { extractAccountId } from '@/lib/auth/validation';
 import { paths } from '@/types/openapi';
+import { validation } from './validation';
+import { PrismaClient } from '@prisma/client';
 
 type RequestBody = paths['/stock/items']['post']['requestBody']['content']['application/json'];
 
@@ -18,7 +20,25 @@ export async function POST(request: NextRequest) {
   }
 
   const requestbody: RequestBody = await request.json();
-  console.debug({ accountId: accountId, requestbody: requestbody });
+  const validStockItem = validation(requestbody);
+  if (validStockItem.kind == 'error') {
+    return NextResponse.json(
+      { error: validStockItem.error.message },
+      { status: 400 }
+    );
+  }
+
+  const prisma = new PrismaClient();
+  const stockItemEvent = await prisma.stockItemEvent.create({
+    data: {
+      aggregateId: crypto.randomUUID(),
+      eventType: 'Created',
+      eventPayload: JSON.stringify({
+        accountId: accountId.value,
+        ...validStockItem.value.body
+      }),
+    }
+  });
 
   return NextResponse.json({ message: 'success' }, { status: 201 });
 }
