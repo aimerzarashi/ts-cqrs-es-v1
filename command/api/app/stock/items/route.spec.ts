f0a4c4cc964afcc812ab06f377923d835f901ee5
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-04 10:15:21 +0900

    ✅ added input validation & persistence

--- command/api/app/stock/items/route.spec.ts
+++ command/api/app/stock/items/route.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
+import { PrismaClient } from '@prisma/client';
 
 const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4T3VJcFluQW1acndFVXBESFlSRkU5NzZGQThUcWs1QmZNMURNZEl2S1o0In0.eyJleHAiOjE2OTg5NzE5MzMsImlhdCI6MTY5ODk3MTYzMywianRpIjoiNTNiMzc2ZmYtODU1Yy00ZWFkLWFjMjUtODQ1OTBmYjY3ZWYwIiwiaXNzIjoiaHR0cHM6Ly9pYW0tcHJvdmlkZXIuYWltZXJ6YXJhc2hpLmNvbS9yZWFsbXMvYWltZXJ6YXJhc2hpIiwiYXVkIjpbImNvbW1hbmQiLCJwb3N0Z3JhcGhpbGUiLCJhY2NvdW50Il0sInN1YiI6IjAwYzE4NGU2LWIyNWMtNGUwOS05MTcxLWUwYzEzZDNkYjhkMyIsInR5cCI6IkJlYXJlciIsImF6cCI6IndlYnNpdGUtdWkiLCJzZXNzaW9uX3N0YXRlIjoiYTU1MmQ1YmUtN2E4OS00YjY4LThlYzQtNTdiZTg5ZTBjYmRkIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtYWltZXJ6YXJhc2hpIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiYTU1MmQ1YmUtN2E4OS00YjY4LThlYzQtNTdiZTg5ZTBjYmRkIiwiYWNjb3VudF9pZCI6IjMyODllYjg4LWM2MDYtNDhiYS1iZjYxLTE0Mzg0MDc2NzQ1NyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiIyMDIzMTEwMjE5MzYwMDAwMEBleGFtcGxlLmNvbSIsImVtYWlsIjoiMjAyMzExMDIxOTM2MDAwMDBAZXhhbXBsZS5jb20ifQ.ESqnfX6jpaG6HLgSHIzjp8u1lCic7yQ8XIlf8-RRlZNl_3-Q32uAIy-G2DbuN4b8JdqU53bl0ZeGZHbCrcH6Ygb6eax1e44FGww7kDNehecwFm5YojvQT0VTuFXz9gOvqo4o5VxQ7JZmi9c0NbKPqMrZ9PbeVPUXjDRSjMOG5xqM1m0f7s-MHXCaOBqN2xGfxoiev39E1OhlNsF_iPN-s3-F86wSwasOpbcBRkDnXKo2-olBtV2ZP0grFmdbbXeh-jahNkLSgg0nMJ3YFkexa_yKkbEl9h8Qr33chOhJYgd-N7kcSDcGvoeYpWyCow6r9VQwcO-GaBHGyrE1hZUiow';
 
@@ -69,7 +70,7 @@ describe("POST", () => {
       "メッセージが期待通りではありません");
   });
 
-  it("正常系", async () => {
+  it("異常系④", async () => {
     const response = await fetch('http://localhost:3000/stock/items',
       {
         method: "POST",
@@ -78,6 +79,28 @@ describe("POST", () => {
           "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify({}),
+      });
+
+    // レスポンスのステータスコードとメッセージを検証するアサーションを行う
+    assert.strictEqual(
+      response.status,
+      400,
+      "ステータスコードが期待通りではありません");
+    assert.deepStrictEqual(
+      JSON.parse(await response.text()),
+      { error: "name is required" },
+      "メッセージが期待通りではありません");
+  });
+
+  it("正常系", async () => {
+    const response = await fetch('http://localhost:3000/stock/items',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({ name: crypto.randomUUID() }),
       });
 
     // レスポンスのステータスコードとメッセージを検証するアサーションを行う
