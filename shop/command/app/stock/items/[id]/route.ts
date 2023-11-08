5e3472f9d24f549e5c4fc008c48b489ae1540b57
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-08 21:53:57 +0900

    🔨 added StockItem put method

---
+++ shop/command/app/stock/items/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from 'next/headers';
+import { extractAccountId } from '@/lib/auth/validation';
+import { paths } from '@/types/openapi';
+
+type RequestBody = paths['/stock/items']['put']['requestBody']['content']['application/json'];
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+  const headersList = headers();
+  const authorization = headersList.get('authorization');
+
+  const accountId = extractAccountId(authorization);
+  if (accountId.kind == 'error') {
+    console.error(accountId.error);
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 401 }
+    );
+  }
+
+  console.debug(params.id);
+
+  console.debug(await request.json());
+
+  return NextResponse.json({ message: 'success' }, { status: 201 });
+}
