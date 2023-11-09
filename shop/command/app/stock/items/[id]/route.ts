e0898f1dec55072e4779fc965864f38997205d28
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-09 21:17:27 +0900

    🔨 Added specification to persist stock item event

--- shop/command/app/stock/items/[id]/route.ts
+++ shop/command/app/stock/items/[id]/route.ts
@@ -1,6 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers';
 import { extractAccountId } from '@/lib/auth/validation';
+import { paths } from '@/schmemas/stock';
+import { PrismaClient } from '@prisma/client';
+import { validation } from '@/app/stock/items/validation';
+
+type RequestBody = paths['/stock/items/{id}']['put']['requestBody']['content']['application/json'];
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   const headersList = headers();
@@ -15,9 +20,45 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
     );
   }
 
-  console.debug(params.id);
+  const requestbody: RequestBody = await request.json();
+  const validStockItem = validation(requestbody);
+  if (!validStockItem.success) {
+    console.error(validStockItem.error);
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 400 }
+    );
+  }
 
-  console.debug(await request.json());
+  const prisma = new PrismaClient();
+  if (! await prisma.stockItemEvent.findFirst({
+    where: {
+      aggregateId: params.id
+    }
+  })) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 400 }
+    );
+  };
+
+  const stockItemEvent = await prisma.stockItemEvent.create({
+    data: {
+      aggregateId: params.id,
+      eventType: 'Updated',
+      eventPayload: JSON.stringify({
+        accountId: accountId.value,
+        ...validStockItem.value.body
+      }),
+    }
+  });
+  if (!stockItemEvent) {
+    console.error(new Error('failed to create stock item event'));
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 500 }
+    );
+  }
 
   return NextResponse.json({ message: 'success' }, { status: 200 });
 }
