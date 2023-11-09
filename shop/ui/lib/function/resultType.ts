0a73a95dab27c33e5234354aa6799f1156c9b290
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-09 20:38:43 +0900

    🔨 renamed from result to resultType

--- shop/ui/lib/function/resultType.ts
+++ shop/ui/lib/function/resultType.ts
@@ -1,12 +1,12 @@
 // 成功した結果を表す型
 export type SuccessResult<T> = {
-  kind: "success";
+  success: true;
   value: T;
 };
 
 // エラーを表す型
 export type ErrorResult = {
-  kind: "error";
+  success: false;
   error: Error;
 };
 
@@ -15,17 +15,17 @@ export type Result<T> = SuccessResult<T> | ErrorResult;
 
 // 成功した結果を作成するヘルパー関数
 export function createSuccess<T>(value: T): SuccessResult<T> {
-  return { kind: "success", value };
+  return { success: true, value };
 }
 
 // エラーを作成するヘルパー関数
 export function createError(error: Error): ErrorResult {
-  return { kind: "error", error };
+  return { success: false, error };
 }
 
 // パイプラインを実現するpipe関数
 export function pipe<T, R>(value: T, ...fns: ((x: T) => Result<R>)[]): Result<R> {
   return fns.reduce((result: Result<any>, fn) => {
-    return result.kind === "success" ? fn(result.value) : result;
+    return result.success ? fn(result.value) : result;
   }, createSuccess(value));
 }
