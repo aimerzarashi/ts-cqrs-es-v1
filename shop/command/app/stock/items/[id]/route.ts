f4c8df8cd8c11e3eb326d5baa9a9850269d4a11e
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-20 22:26:10 +0900

    🔨 added aggregate processes

--- shop/command/app/stock/items/[id]/route.ts
+++ shop/command/app/stock/items/[id]/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers';
-import { extractAccountId } from '@/lib/auth/validation';
-import { paths } from '@/schemas/stockItem';
+import { extractAccountId } from '@/lib/headers/auth';
+import { paths } from '@/schemas/openapi/stockItem';
+import { generate, update, StockItemUpdateCommand } from "@/lib/domain/stock/items/aggregate";
+import { findEvents, storeEvent } from "@/lib/domain/stock/items/repository";
 
 type RequestBody = paths['/stock/items/{id}']['put']['requestBody']['content']['application/json'];
 
@@ -10,13 +12,52 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
   const authorization = headersList.get('authorization');
 
   // アカウントIDを取得
-  const accountId = extractAccountId(authorization);
-  if (!accountId.success) {
-    console.error(accountId.error);
+  const extractAccountIdResult = extractAccountId(authorization);
+  if (!extractAccountIdResult.success) {
+    console.error(extractAccountIdResult.error);
     return NextResponse.json(
       { message: 'failed' },
       { status: 401 }
     );
+  }
+
+  const stockItemUpdateCommand: StockItemUpdateCommand = {
+    name: "test2",
+  };
+
+  const findEventsResult = await findEvents(params.id);
+  if (!findEventsResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 400 }
+    )
+  }
+
+  const foundDomainEvents = findEventsResult.value;
+  const generateResult = generate(foundDomainEvents);
+  if (!generateResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 400 }
+    )
+  }
+
+  const aggregate = generateResult.value;
+  const updateResult = update(aggregate, stockItemUpdateCommand);
+  if (!updateResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 500 }
+    )
+  }
+
+  const { domainEvent } = updateResult.value;
+  const storeEventResult = await storeEvent(domainEvent);
+  if (!storeEventResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 500 }
+    )
   }
 
   return NextResponse.json({ message: 'success' }, { status: 200 });
--- shop/command/app/stock/items/route.ts
+++ shop/command/app/stock/items/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers';
-import { extractAccountId } from '@/lib/auth/validation';
-import { paths } from '@/schemas/stockItem';
+import { extractAccountId } from '@/lib/headers/auth';
+import { paths } from '@/schemas/openapi/stockItem';
+import { create, StockItemCreateCommand } from "@/lib/domain/stock/items/aggregate";
+import { storeEvent } from "@/lib/domain/stock/items/repository";
 
 type RequestBody = paths['/stock/items']['post']['requestBody']['content']['application/json'];
 
@@ -9,15 +11,37 @@ export async function POST(request: NextRequest) {
   const headersList = headers();
   const authorization = headersList.get('authorization');
 
-  const accountId = extractAccountId(authorization);
-  if (!accountId.success) {
-    console.error(accountId.error);
+  const extractAccountIdResult = extractAccountId(authorization);
+  if (!extractAccountIdResult.success) {
+    console.error(extractAccountIdResult.error);
     return NextResponse.json(
       { message: 'failed' },
       { status: 401 }
     );
   }
 
+  const accountId = extractAccountIdResult.value;
+  const stockItemCreateCommand: StockItemCreateCommand = {
+    id: crypto.randomUUID(),
+    name: "test1",
+    accountId: accountId,
+  };
+  const createResult = create(stockItemCreateCommand);
+  if (!createResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 400 }
+    )
+  }
+
+  const { domainEvent } = createResult.value;
+  const storeEventResult = await storeEvent(domainEvent);
+  if (!storeEventResult.success) {
+    return NextResponse.json(
+      { message: 'failed' },
+      { status: 500 }
+    )
+  }
 
   return NextResponse.json({ message: 'success' }, { status: 201 });
 }
