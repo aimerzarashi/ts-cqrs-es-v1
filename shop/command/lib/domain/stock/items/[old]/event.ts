03ad40ea96ec9736e8e48a297a716c5c7eb95350
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-20 22:00:14 +0900

    🔥 I deleted it because the refactoring of the aggregation was completed.

--- shop/command/lib/domain/stock/items/[old]/event.ts
+++
@@ -1,6 +0,0 @@
-import { components } from "@/schemas/asyncapi/stockItemEvent";
-
-export type StockItemEvent = StockItemCreatedEvent | StockItemUpdatedEvent;
-
-export type StockItemCreatedEvent = components["schemas"]["StockItemCreatedEvent"];
-export type StockItemUpdatedEvent = components["schemas"]["StockItemUpdatedEvent"];
