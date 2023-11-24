1e04d4ddf01185d148a644219ebe7dab467e2ed0
aimerzarashi <git.aimerzarashi@gmail.com>
2023-11-24 22:31:46 +0900

    🔨 added stock/items back link

--- shop/ui/app/stock/items/[id]/page.tsx
+++ shop/ui/app/stock/items/[id]/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <main>
       <div>{params.id}</div>
+      <Link href="/stock/items">Back</Link>
     </main>
   );
 }
